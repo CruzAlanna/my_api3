49572466ad754157b966322ca89260f7c4101aa9
CruzAlanna <lannikcru@gmail.com>
2025-02-24 20:38:41 +1000

    Added migration adding email: to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :assignments
   has_many :projects, through: :assignments
+
+  #Advanced Validations
+  validates 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_24_093715) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_24_103016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_24_093715) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   add_foreign_key "assignments", "projects"
