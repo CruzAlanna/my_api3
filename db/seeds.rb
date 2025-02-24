e6e12650174d5d5584b606259173d99cdad20bee
CruzAlanna <lannikcru@gmail.com>
2025-02-24 21:04:58 +1000

    Added data for Gadget in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,6 @@ user = User.create!(name: "Aaron Rivers")
 project = Project.create!(name: "Project X")
 
 Assignment.create!(user: user, project: project)
+
+Gadget.create!(name: "Test Gadget", sku: "GAD-001")
+Gadget.create!(name: "Cool Gizmo", sku: "GAD-XYZ")
